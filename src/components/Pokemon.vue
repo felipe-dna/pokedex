395b815b96b60b3f5b7103ca68f28a8e70bc88ef
Felipe Amador <felipedavidamador@gmail.com>
2022-09-06 23:58:36 -0300

    feat: set the real data

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,10 +1,10 @@
 <template>
   <a class="group">
     <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-      <img :src="data.sprites.front_default" class="h-full w-full object-cover object-center group-hover:opacity-75" />
+      <img :src="data?.sprites?.front_default" class="h-full w-full object-cover object-center group-hover:opacity-75" />
     </div>
-    <h3 class="mt-4 text-sm text-gray-700">{{ data.id }}</h3>
-    <p class="mt-1 text-lg font-medium text-gray-900">{{ data.name }}</p>
+    <h3 class="mt-4 text-sm text-gray-700">{{ data?.id }}</h3>
+    <p class="mt-1 text-lg font-medium text-gray-900">{{ data?.name }}</p>
   </a>
 </template>
 
