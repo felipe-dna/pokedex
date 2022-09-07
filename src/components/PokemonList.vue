c6f3f7053d668388057b940c887f072f6a6a6d86
Felipe Amador <felipedavidamador@gmail.com>
2022-09-06 23:22:21 -0300

    feat: apply styles in pokemom list

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import {onBeforeMount, ref} from "vue";
 import {loadPokemon} from "../services/api";
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 const pokemonList = ref()
 
@@ -10,16 +12,69 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <div>
-    <h1>Pokemon List</h1>
+  <div class="relative overflow-hidden bg-white">
+    <div class="mx-auto max-w-7xl">
+      <div class="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+        <svg class="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+          <polygon points="50,0 100,0 50,100 0,100" />
+        </svg>
 
-    <ul>
-      <li v-for="pokemon in pokemonList" :key="pokemon.name">
-        {{pokemon}}
-      </li>
-    </ul>
+        <Popover>
+          <div class="relative px-4 pt-6 sm:px-6 lg:px-8">
+            <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+              <div class="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
+                <div class="flex w-full items-center justify-between md:w-auto">
+                  <a href="#">
+                    <span class="sr-only">Workflow</span>
+                    <img alt="Workflow" class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" />
+                  </a>
+                  <div class="-mr-2 flex items-center md:hidden">
+                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span class="sr-only">Open main menu</span>
+                      <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+                    </PopoverButton>
+                  </div>
+                </div>
+              </div>
+              <div class="hidden md:ml-10 md:block md:space-x-8 md:pr-4">
+              </div>
+            </nav>
+          </div>
+
+          <transition enter-active-class="duration-150 ease-out" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
+            <PopoverPanel focus class="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden">
+              <div class="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+                <div class="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="" />
+                  </div>
+                  <div class="-mr-2">
+                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span class="sr-only">Close main menu</span>
+                      <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                    </PopoverButton>
+                  </div>
+                </div>
+                <div class="space-y-1 px-2 pt-2 pb-3">
+                </div>
+              </div>
+            </PopoverPanel>
+          </transition>
+        </Popover>
+      </div>
+    </div>
+    <div>
+      <h1>Pokemon List</h1>
+
+      <ul>
+        <li v-for="pokemon in pokemonList" :key="pokemon.name">
+          {{pokemon}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style scoped>
+
 </style>
