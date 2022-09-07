90f0ba0f0bbdadc2cbb944c284b8e203e32aef6e
Felipe Amador <felipedavidamador@gmail.com>
2022-09-06 23:07:40 -0300

    feat: lista the pokemons in the PokemonList component

--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -1,12 +1,25 @@
-<script setup>
+<script setup lang="ts">
+import {onBeforeMount, ref} from "vue";
+import {loadPokemon} from "../services/api";
+
+const pokemonList = ref()
+
+onBeforeMount(async () => {
+  pokemonList.value = await  loadPokemon()
+})
 </script>
 
 <template>
   <div>
     <h1>Pokemon List</h1>
+
+    <ul>
+      <li v-for="pokemon in pokemonList" :key="pokemon.name">
+        {{pokemon}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
-
 </style>
--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,4 +1,4 @@
-async function loadPokemon() {
+export async function loadPokemon() {
   const response = await fetch('https://pokeapi.co/api/v2/pokemon')
   const { results } = await response.json()
 
